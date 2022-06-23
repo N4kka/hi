cda872adc87d00fc4de44e8c315646ebd502e4cb
Nicola Sarli <you@example.com>
2022-06-23 12:51:50 +0200

    css link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,26 @@
-<header>
-    <ul>
-        <li>
-            <a href=" {{route('about')}} ">About</a>
-        </li>
-    </ul>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li>
+                <a href=" {{ route('about') }} ">About</a>
+            </li>
+        </ul>
+    </header>
+</body>
+
+</html>
+
 
 <h1>Ciao {{ $name }} {{ $lastname }}, sono una pagina home</h1>
 
